6e03aad9a17921adb1e9663c49a0f6aab10ebe74
Oliver Peate <oli@shippedcode.com>
2021-02-22 11:12:37 +0000

    Turn Bluetooth ON when app launched
    Counter action to Bluetooth OFF during shutdown:
    3aced9eeaf59bf2882594f608202910f5e28f4aa

--- Bluesnooze/AppDelegate.swift
+++ Bluesnooze/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         initStatusItem()
         setLaunchAtLoginState()
         setupNotificationHandlers()
+        setBluetooth(powerOn: true)
     }
 
     // MARK: Click handlers
